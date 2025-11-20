617f2b7e35ef93333a3f99e4f78178190a4551e4
ashishka13 <ashishka13@gmail.com>
2025-11-20 12:56:12 +0530

    reset and restart

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import "url-shortner/handler"
-
-var Url = "https://youtube.com/@SpaceX"
-
-var UrlMap = make(map[string]string)
-
-const TinyUrlPrefix = "tu.ai/"
-
 func main() {
-	UrlMap["gu"] = "https://google.com"
-	handler.StartController(UrlMap)
+
 }
